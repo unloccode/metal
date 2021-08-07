2c80f8f8ecc16dd9e47e5fcdfe0a050cad169018
unloccode <unloccode@gmail.com>
2021-08-07 16:59:41 +0300

    Added user and role models, and updated the db config for sequelize with modes

--- config/db.config.js
+++ config/db.config.js
@@ -22,3 +22,20 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 //import models
+db.user = require('../models/user.model.js')(sequelize, Sequelize);
+db.role = require('../models/role.model.js')(sequelize, Sequelize);
+//define relationship
+db.role.belongsToMany(db.user, {
+    through: 'user_roles',
+    foreignKey: 'roleId',
+    otherKey: 'userId'
+});
+db.user.belongsToMany(db.role, {
+    through: 'user_roles',
+    foreignKey: 'userId',
+    otherKey: 'roleId'
+});
+
+db.ROLES = ['user', 'admin', 'moderator'];
+
+module.exports = db;
