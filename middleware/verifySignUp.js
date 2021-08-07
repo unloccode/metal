ff96de28beac82b2e13f875220b35afad572b76e
unloccode <unloccode@gmail.com>
2021-08-07 17:46:35 +0300

    Added a feature to check for data authenticity

---
+++ middleware/verifySignUp.js
@@ -0,0 +1,54 @@
+const db = require('../config/db.config.js');
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateUsernameOrEmail = (req, res, next) =>{
+    //username
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user=>{
+        if(user){
+            res.status(400).send({
+                message: "Failed! Username already in use!"
+            });
+            return;
+        }
+        //email
+        User.findOne({
+            where: {
+                email: req.body.email
+            }
+        }).then(user=>{
+            if(user){
+                res.status(400).send({
+                    message: "Failed! Email already in use!"
+                });
+                return;
+            }
+            next();
+        });
+    });
+};
+
+checkRolesExisted = (req, res, next) => {
+    if(req.body.roles){
+        for(let i=0; i<req.body.roles.length; i++){
+            if(!ROLES.includes(req.body.roles[i])){
+                res.status(400).send({
+                    message: "Failed! Role does not exist =" + req.body.roles[i]
+                });
+                return;
+            }
+        }
+    }
+    next();
+};
+
+const verifySignUp = {
+    checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail,
+    checkRolesExisted: checkRolesExisted
+};
+
+module.exports = verifySignUp;
--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//in app imports
+const db = require('./config/db.config.js');
+const Role = db.role;
+
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
+
+const corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+//parse requests of content-type -application/json
+app.use(bodyParser.json());
+//parse requests of content-type -application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
 
 //simple route
 app.get('/', (req, res)=>{
@@ -11,3 +29,24 @@ app.get('/', (req, res)=>{
 app.listen(PORT, ()=>{
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
+//some I N I T S
+db.sequelize.sync({force: true}).then(()=>{
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial(){
+    Role.create({
+        id: 1,
+        name: 'user'
+    });
+    Role.create({
+        id: 2,
+        name: 'moderator'
+    });
+    Role.create({
+        id: 3,
+        name: 'admin'
+    });
+}
